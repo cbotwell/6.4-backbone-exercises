f1785780df85e7102c3f2e573faddc1b72fc60e5
Corbin Otwell <cbotwell@gmail.com>
2015-08-10 20:33:54 -0500

    added edit to router and styled edit

--- assets/js/router/router.js
+++ assets/js/router/router.js
@@ -1,19 +1,31 @@
-var AppRouter = Backbone.Router.extend ({
+var AppRouter = Backbone.Router.extend({
   initialize: function() {
-    this.template = AppTemplates.blog;
     this.collection = new BlogPosts();
   },
 
   routes: {
-    ':id(/)': 'post'
+    ':id(/)': 'post',
+    ':id/edit': 'edit',
   },
 
   post: function(id) {
+    this.template = AppTemplates.blog;
     var _this = this;
+
     this.collection.fetch().then(function() {
       var html = _this.template(_this.collection.get(id).toJSON());
       $('#target').html(html);
     });
-  }
+  },
+
+  edit: function(id) {
+    this.template = AppTemplates.edit;
+    var _this = this;
+
+    this.collection.fetch().then(function() {
+      var html = _this.template(_this.collection.get(id).toJSON());
+      $('#target').html(html);
+    });
+  },
 
 });
